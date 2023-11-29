c678594eede250d07b07640b5422f21498a033a4
EricMTodd <eterictodd@gmail.com>
2023-11-29 12:07:25 -0700

    Assignment complete.

--- program.cpp
+++ program.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,6 +16,44 @@ struct computerType
 };
 
 int main() {
-    // Write your main here
+    struct computerType newComputer;
+
+    cout << "Please input computer manufacturer: ";
+    getline(cin, newComputer.manufacturer);
+    // cout << endl;
+
+    cout << "Please input computer model type: ";
+    getline(cin, newComputer.modelType);
+    // cin >> computerType.modelType;
+    // cout << endl;
+
+    cout << "Please input computer processor type: ";
+    getline(cin, newComputer.processorType);
+    // cout << endl;
+
+    cout << "Please input RAM size: ";
+    cin >> newComputer.ram;
+    // cout << endl;
+
+    cout << "Please input hard drive size: ";
+    cin >> newComputer.hardDriveSize;
+    // cout << endl;
+
+    cout << "Please input the year built: ";
+    cin >> newComputer.yearBuilt;
+    // cout << endl;
+
+    cout << "Please input the price of the computer: ";
+    cin >> newComputer.price;
+    // cout << endl;
+
+    cout << "Manufacturer: " << newComputer.manufacturer << endl;
+    cout << "Model: " << newComputer.modelType << endl;
+    cout << "Processor: " << newComputer.processorType << endl;
+    cout << "Ram: " << newComputer.ram << endl;
+    cout << "Hard Drive Size: " << newComputer.hardDriveSize << endl;
+    cout << "Year Built: " << newComputer.yearBuilt << endl;
+    cout << "Price: $" << newComputer.price << showpoint << fixed << endl;
+
     return 0;
 }
